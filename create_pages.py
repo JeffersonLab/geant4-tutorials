ad54750fe10a785ac0b7cc34922a5ddd8b6cc4ee
mungaro <maurizio.ungaro@cnu.edu>
2024-02-14 11:24:04 -0500

    closer to hands0n1 completion

--- create_pages.py
+++ create_pages.py
@@ -14,18 +14,15 @@ g4varsion = "11.2.0"
 
 # Get the current directory
 current_dir = os.getcwd()
-print("Current directory: ", current_dir)
+print(" > Current directory: ", current_dir)
 
-# Get the parent directory
-parent_dir = os.path.dirname(current_dir)
-print("Parent directory: ", parent_dir)
 
 # Get the list of subdirectories starting with HandsOn
 subdirs = [d for d in os.listdir(current_dir) if os.path.isdir(d) and d.startswith("HandsOn1")]
 
 # Get the content of header.html and footer.html
-header_file = os.path.join(parent_dir, "header.html")
-footer_file = os.path.join(parent_dir, "footer.html")
+header_file = os.path.join(current_dir, "header.html")
+footer_file = os.path.join(current_dir, "footer.html")
 with open(header_file, "r") as file:
 	header = file.read()
 with open(footer_file, "r") as file:
@@ -33,19 +30,34 @@ with open(footer_file, "r") as file:
 
 # Add the header and footer to the content of index.html in each subdirectory
 for subdir in subdirs:
+	print("\n > Subdir: ", subdir)
 	index_file = os.path.join(current_dir, subdir, "index.html")
 	with open(index_file, "w") as file:
 		file.write(header)
+
+		navigation_file = os.path.join(current_dir, subdir, "navigation.html")
+		with open(navigation_file, "r") as nav_file:
+			navigation = nav_file.read()
+			file.write(navigation)
+
+		content_file = os.path.join( current_dir, subdir, "content.dat" )
+		with open( content_file, "r" ) as subdir_file:
+			content = subdir_file.read()
+
+		# loop over each line of content.dat
+		content_list = content.split( "\n" )
+
+		# write the content of each file listed in content.dat to index.html
+		for file_name in content_list:
+			if file_name != "":
+				file_path = os.path.join( current_dir, subdir, file_name )
+				print("   > Adding: ", file_path )
+				with open( file_path, "r" ) as subdir_file:
+					file_content = subdir_file.read()
+					file.write( file_content )
+					# add a line break after each file
+					file.write( "<br/><hr/>" )
+
 		file.write(footer)
 
-# Loop over the subdirectories
-for subdir in subdirs:
-	# Get the content of content.dat
-	content_file = os.path.join(current_dir, subdir, "content.dat")
-	with open(content_file, "r") as file:
-		content = file.read()
-
-	# loop over each line of content.dat
-	content_list = content.split("\n")
-	print("Content list: ", content_list)
-
+print("\n > Done!\n")
--- main.js
+++ main.js
@@ -1,24 +1,14 @@
-var g4version = "11.2.0";
+let g4version = "11.2.0";
 
-window.onload = function () {
-
-	// sets all tags with name "g4version" to the value of g4version
-	// not just the first one
-	// not just in the current document but in all included html files
-	var g4versions = document.getElementsByName("g4version");
-	for (var i = 0; i < g4versions.length; i++) {
-		g4versions[i].innerHTML = g4version;
-	}
-};
 
 // When the user scrolls the page, execute myFunction
 window.onscroll = function() { keep_top_bar() };
 
 // Get the navbar
-var topnav = document.getElementById("topnav");
+let topnav = document.getElementById("topnav");
 
 // Get the offset position of the navbar
-var sticky = topnav.offsetTop;
+let sticky = topnav.offsetTop;
 
 // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function keep_top_bar() {
@@ -27,4 +17,5 @@ function keep_top_bar() {
   } else {
     topnav.classList.remove("sticky");
   }
+
 }
